9cf58b967cc4de2908fcfd0de039363135028120
Alex Gi <alex@alexgi.alexgi.com>
2026-02-21 23:24:46 +0700

    missing stuffs

--- terraform/modules/gcp_infra/main.tf
+++
@@ -1,4 +0,0 @@
-resource "google_storage_bucket" "linkedin-scrapping-bucket" {
-  name     = var.bucket_name
-  location = var.location
-}
--- terraform/modules/gcp_infra/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  default     = "linkedin-scrapping-gcp-bucket"
-  description = "gcp bucket name"
-}
-
-variable "location" {
-  default     = "US"
-  description = "location of the bucket"
-}
