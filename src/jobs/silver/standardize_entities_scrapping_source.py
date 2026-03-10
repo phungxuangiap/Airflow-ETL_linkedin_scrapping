d25a49d9a89f5be0450d718244125323d168cda0
Alex Gi <alex@alexgi.alexgi.com>
2026-03-10 11:36:59 +0700

    update scrapping standardize logic

--- src/jobs/silver/standardize_entities_scrapping_source.py
+++ src/jobs/silver/standardize_entities_scrapping_source.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 
 from models.silver import CompanySilver, JobSilver
-def standardize_entities_api_source(
+def standardize_entities_scrapping_source(
     load_date: str = datetime.now().strftime("%Y-%m-%d")
 ):
     con = duckdb.connect(database='temp_duckdb.duckdb')
@@ -18,7 +18,7 @@ def standardize_entities_api_source(
         SET s3_use_ssl = false;
         SET s3_url_style = 'path';
     """)
-    bronze_path = f"s3://{os.getenv('BUCKET')}/BRONZE/api_data/jobs/load_date={load_date}/*.jsonl"
+    bronze_path = f"s3://{os.getenv('BUCKET')}/BRONZE/crawler_data/linkedin/jobs/load_date={load_date}/*.jsonl"
     try:
         query_standardize_jobs = f"""
             SELECT 
@@ -26,7 +26,7 @@ def standardize_entities_api_source(
                 COALESCE(
                     NULLIF(
                         REGEXP_REPLACE(
-                            LOWER(TRIM(company_name)), 
+                            LOWER(TRIM(company)), 
                             '\s*(,?\s*(inc|co|ltd|llc|corp|tnhh|cổ phần|group|joint stock|ltd\.?|co\.?))\s*$', 
                             '', 
                             'g'
@@ -36,19 +36,19 @@ def standardize_entities_api_source(
                     'Unknown'
                 ) AS {JobSilver.company_id},
                 COALESCE(source, 'Unknown') as {JobSilver.source_name},
-                COALESCE(remote_derived, 'N/A') as {JobSilver.location_type},
-                COALESCE(employment_type[0], 'Unknown') as {JobSilver.employment_type},
+                COALESCE(location_working_type, 'N/A') as {JobSilver.location_type},
+                COALESCE(working_type, 'Unknown') as {JobSilver.employment_type},
                 COALESCE(date_posted, 'Unknown') as {JobSilver.date_posted},
                 list_filter(
                     {TECH_LIST}, 
                     x -> regexp_matches(lower(description), '\b' || x || '\b')
                 ) as {JobSilver.techstacks},
-                COALESCE(url, 'Unknown') as {JobSilver.job_url},
-                COALESCE(salary_raw, 'Unknown') as {JobSilver.salary},
-                COALESCE(seniority, 'Unknown') as {JobSilver.level},
+                COALESCE(job_url, 'Unknown') as {JobSilver.job_url},
+                COALESCE(salary, 'Unknown') as {JobSilver.salary},
+                COALESCE(level, 'Unknown') as {JobSilver.level},
                 list_filter(
                     {ROLE_LIST}, 
-                    x -> regexp_matches(lower(title), '\b' || x || '\b')
+                    x -> regexp_matches(lower(role), '\b' || x || '\b')
                 ) as {JobSilver.role},
                 COALESCE(CAST(number_applicants AS INTEGER), 0) as {JobSilver.number_applicants},
                 CURRENT_TIMESTAMP as processed_at
@@ -61,7 +61,7 @@ def standardize_entities_api_source(
                 COALESCE(
                     NULLIF(
                         REGEXP_REPLACE(
-                            LOWER(TRIM(company_name)), 
+                            LOWER(TRIM(company)), 
                             '\s*(,?\s*(inc|co|ltd|llc|corp|tnhh|cổ phần|group|joint stock|ltd\.?|co\.?))\s*$', 
                             '', 
                             'g'
@@ -70,10 +70,10 @@ def standardize_entities_api_source(
                     ), 
                     'Unknown'
                 ) AS {CompanySilver.id},
-                COALESCE(organization, 'Unknown') as {CompanySilver.name},
-                COALESCE(linkedin_org_industry, 'Unknown') as {CompanySilver.industry},
-                COALESCE(linkedin_org_size, 'Unknown') as {CompanySilver.company_size},
-                COALESCE(linkedin_org_headquarters, 'Unknown') as {CompanySilver.location}
+                COALESCE(company, 'Unknown') as {CompanySilver.name},
+                COALESCE('Unknown') as {CompanySilver.industry},
+                COALESCE('Unknown') as {CompanySilver.company_size},
+                COALESCE(company_location, 'Unknown') as {CompanySilver.location}
             FROM read_json_auto('{bronze_path}', ignore_errors=True, format='newline_delimited')
         """
         companies_arrow_table = con.execute(query_standardize_companies).fetch_arrow_table()
